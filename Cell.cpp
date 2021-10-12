1460765f563bdbec37c87ff7a7535ab9026871ac
Andrey Kalkutin <CosmoStar@protonmail.com>
2021-10-12 21:39:36 +0500

    More comments

--- Cell.cpp
+++ Cell.cpp
@@ -2,17 +2,28 @@
 
 Cell::Cell()
 {
+	/* ----Default constructor----
+	 *
+	 * `value` - current value of the cell
+	 * `changed` - `true` if cell's value has to be changed */
+
 	value = false;
 	changed = false;
 }
 
 void 
 Cell::assign(const bool value)
+/* Set `changed` to `true` if cell's value has to be changed */
 { changed = (this->value != value); }
 
 void
 Cell::assign(const int neighboursCount)
 {
+	/* Decide whether cell's value has to be changed
+	 * according to it's neighbours' count 
+	 * If cell is alive and has not 2 or 3 neighbours - change value 
+	 * If cell is dead and has 3 neigbours - change value */
+
 	if (value)
 	{
 		if (neighboursCount != 2 && neighboursCount != 3)
@@ -27,7 +38,9 @@ Cell::assign(const int neighboursCount)
 
 void 
 Cell::update()
-{ 
+{
+	/* Set new cell's current value if it has to be changed */
+
 	if (changed)
 	{
 		value = !value;
@@ -37,4 +50,5 @@ Cell::update()
 
 bool 
 Cell::getValue() const
+/* Get cell's current value */
 { return value; }
